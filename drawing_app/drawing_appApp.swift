080851f561b5e523ee5976fa13e84f68ec6fb99a
Saransh Singhal <singhalsaransh40@gmail.com>
2025-02-11 20:49:08 +0530

    Initial Commit

---
+++ drawing_app/drawing_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawing_appApp.swift
+//  drawing_app
+//
+//  Created by Saransh Singhal on 11/02/25.
+//
+
+import SwiftUI
+
+@main
+struct drawing_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ drawing_appTests/drawing_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  drawing_appTests.swift
+//  drawing_appTests
+//
+//  Created by Saransh Singhal on 11/02/25.
+//
+
+import Testing
+@testable import drawing_app
+
+struct drawing_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
